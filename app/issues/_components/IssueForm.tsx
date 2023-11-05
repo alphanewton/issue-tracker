c289c56a22166eeadc4c0d60e9f9d1849e8029a8
newton <newtonnarzary28@gmail.com>
2023-11-05 19:42:26 +0530

    Change relation mode to prisma

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -5,7 +5,7 @@ import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Issue } from "@prisma/client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Select, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
@@ -35,6 +35,7 @@ function IssueForm({ issue }: { issue?: Issue }) {
         className="space-y-3"
         onSubmit={handleSubmit(async (data) => {
           try {
+            console.log(data);
             setSubmitting(true);
             if (issue) await axios.patch("/api/issues/" + issue.id, data);
             else await axios.post("/api/issues", data);
@@ -53,6 +54,24 @@ function IssueForm({ issue }: { issue?: Issue }) {
           />
         </TextField.Root>
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
+
+        <Select.Root defaultValue={issue?.status} {...register("status")}>
+          <Select.Group>
+            <Select.Trigger placeholder="Status" />
+            <Select.Content>
+              <Select.Item key="OPEN" value="OPEN">
+                Open
+              </Select.Item>
+              <Select.Item key="IN_PROGRESS" value="IN_PROGRESS">
+                In Progress
+              </Select.Item>
+              <Select.Item key="CLOSED" value="CLOSED">
+                Closed
+              </Select.Item>
+            </Select.Content>
+          </Select.Group>
+        </Select.Root>
+        <ErrorMessage>{errors.status?.message}</ErrorMessage>
 
         <Controller
           name="description"
